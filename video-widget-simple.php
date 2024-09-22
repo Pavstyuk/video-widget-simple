45d0369a23ff8ea9325293a968cace72df1bc2b5
Михаил Павстюк <pavstyuk@localhost.localdomain>
2024-09-22 17:39:32 +0700

    update to ver. 0.0.7

--- video-widget-simple.php
+++ video-widget-simple.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Video Widget Simple
-Plugin URI: https://pavstyuk.ru/
+Plugin URI: https://github.com/Pavstyuk/video-widget-simple
 Description: Very Simple plugin to display video widget with presentation of business or any ideas on front page of your WordPress Site.
-Version: 0.0.4
+Version: 0.0.7
 Requires at least: 5.8
 Requires PHP: 5.6.20
 Author: Mikhail Pavstyuk
@@ -17,7 +17,7 @@ if (!function_exists('add_action')) {
     die('Nothing to do. Bye.');
 }
 
-define('VWS_VER', '0.0.3');
+define('VWS_VER', '0.0.7');
 define('VWS_DIR', plugin_dir_path(__FILE__));
 
 require_once VWS_DIR . "widget-functions.php";
@@ -28,3 +28,10 @@ add_action("admin_init", "vws_add_settings");
 add_action('plugin_loaded', 'vws_load_text_domain');
 add_action("admin_menu", "vws_plugin_pages");
 add_action('wp_print_footer_scripts', 'vws_scripts_front');
+
+
+register_activation_hook(__FILE__, "vws_activation");
+register_deactivation_hook(__FILE__, "vws_deactivation");
+
+function vws_activation() {}
+function vws_deactivation() {}
